e61a2de0be441143124306be959ea9d2e6368342
bellambellamy <valentin.rdrguez@gmail.com>
2022-10-10 18:47:47 +0200

    Lunes 10 de Octubre de 2022

---
+++ js/demo.js
@@ -0,0 +1,4 @@
+document.getElementById("name").value = "Valentín";
+document.getElementById("lastName").value = "Rodríguez Cámara";
+
+document.getElementById("avatar").src = "img/deathly_hallows.png";
